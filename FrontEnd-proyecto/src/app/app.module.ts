d30b7fc604dc987776a7de22d044a29fdf605748
vanesaarandavera <vanesaa715@gmail.com>
2022-08-21 00:15:16 -0300

    banner integrado

--- FrontEnd-proyecto/src/app/app.module.ts
+++ FrontEnd-proyecto/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { LogoApComponent } from './componets/logo-ap/logo-ap.component';
+import { SocialComponent } from './componets/social/social.component';
+import { BannerComponent } from './componets/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,

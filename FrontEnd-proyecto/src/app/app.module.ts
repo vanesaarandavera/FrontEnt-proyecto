60157656f52a1b6a193b2cf12885affef9cfe3d8
vanesaarandavera <vanesaa715@gmail.com>
2022-08-22 14:04:38 -0300

    arreglos en components

--- FrontEnd-proyecto/src/app/app.module.ts
+++ FrontEnd-proyecto/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './componets/header/header.component';
-import { LogoApComponent } from './componets/logo-ap/logo-ap.component';
-import { SocialComponent } from './componets/social/social.component';
-import { BannerComponent } from './componets/banner/banner.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    SocialComponent,
-    BannerComponent
+   
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
--- FrontEnd-proyecto/src/app/components/acerca-de/acerca-de.component.spec.ts
+++ FrontEnd-proyecto/src/app/components/acerca-de/acerca-de.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SocialComponent } from './social.component';
+import { AcercaDeComponent } from './acerca-de.component';
 
-describe('SocialComponent', () => {
-  let component: SocialComponent;
-  let fixture: ComponentFixture<SocialComponent>;
+describe('AcercaDeComponent', () => {
+  let component: AcercaDeComponent;
+  let fixture: ComponentFixture<AcercaDeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SocialComponent ]
+      declarations: [ AcercaDeComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SocialComponent);
+    fixture = TestBed.createComponent(AcercaDeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
